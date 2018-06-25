c8f8f30f8b729d0dd84234892c4aa6849b6427e5
Zana Flander <zana.flander@gmail.com>
2018-06-25 10:49:07 +0200

    service worker added

--- js/main.js
+++ js/main.js
@@ -214,3 +214,24 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 		self.markers.push(marker);
 	});
 } */
+
+/* adding service worker */
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./js/sw.js')
+	.then((reg) => {
+		// registration worked
+		if(reg.installing) {
+			console.log('Service worker  is installing.');
+		} else if(reg.waiting) {
+			console.log('Service worker is installed.');
+		} else if(reg.active) {
+			console.log('Service worker is active.');
+		}
+
+		console.log('Registration succeeded. Scope is ' + reg.scope);
+	
+	}).catch((error) => {
+		// registration failed
+		console.log('Registration failed with ' + error);
+	});
+}
